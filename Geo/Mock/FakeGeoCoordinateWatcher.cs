bbafca69acf9feac73caf3d715e992c004be2c2a
ruxa <asd@server.fake>
2013-06-02 18:31:29 +0300

    Mock geo position watcher stuff

--- Geo/Mock/FakeGeoCoordinateWatcher.cs
+++ Geo/Mock/FakeGeoCoordinateWatcher.cs
@@ -1,30 +1,70 @@
 ﻿using System;
 using System.Device.Location;
 
-namespace runner
+namespace runner.Geo.Mock
 {
-    public class FakeGeoCoordinateWatcher : GeoCoordinateWatcher
+    public class FakeGeoCoordinateWatcher : IGeoCoordinateWatcher
     {
-        public FakeGeoCoordinateWatcher(GeoPositionAccuracy accuracy):base(accuracy)
+        public FakeGeoCoordinateWatcher(GeoPositionAccuracy accuracy)
         {
             
         }
 
-        public  void Start()
-        {
-            OnPositionStatusChanged(new GeoPositionStatusChangedEventArgs(GeoPositionStatus.Ready));
+        //public  void Start()
+        //{
+        //    OnPositionStatusChanged(new GeoPositionStatusChangedEventArgs(GeoPositionStatus.Ready));
            
-            OnPositionChanged(new GeoPositionChangedEventArgs<GeoCoordinate>(new GeoPosition<GeoCoordinate>(DateTime.Now, new GeoCoordinate(44.448074, 26.081837))));
-            OnPositionChanged(new GeoPositionChangedEventArgs<GeoCoordinate>(new GeoPosition<GeoCoordinate>(DateTime.Now, new GeoCoordinate(44.447959, 26.082315))));
-            OnPositionChanged(new GeoPositionChangedEventArgs<GeoCoordinate>(new GeoPosition<GeoCoordinate>(DateTime.Now, new GeoCoordinate(44.447924, 26.082518))));
-            OnPositionChanged(new GeoPositionChangedEventArgs<GeoCoordinate>(new GeoPosition<GeoCoordinate>(DateTime.Now, new GeoCoordinate(44.448231, 26.082679))));
-            OnPositionChanged(new GeoPositionChangedEventArgs<GeoCoordinate>(new GeoPosition<GeoCoordinate>(DateTime.Now, new GeoCoordinate(44.450681, 26.084096))));   
-            OnPositionChanged(new GeoPositionChangedEventArgs<GeoCoordinate>(new GeoPosition<GeoCoordinate>(DateTime.Now, new GeoCoordinate(44.451187, 26.084364))));
+        //    OnPositionChanged(new GeoPositionChangedEventArgs<GeoCoordinate>(new GeoPosition<GeoCoordinate>(DateTime.Now, new GeoCoordinate(44.448074, 26.081837))));
+        //    OnPositionChanged(new GeoPositionChangedEventArgs<GeoCoordinate>(new GeoPosition<GeoCoordinate>(DateTime.Now, new GeoCoordinate(44.447959, 26.082315))));
+        //    OnPositionChanged(new GeoPositionChangedEventArgs<GeoCoordinate>(new GeoPosition<GeoCoordinate>(DateTime.Now, new GeoCoordinate(44.447924, 26.082518))));
+        //    OnPositionChanged(new GeoPositionChangedEventArgs<GeoCoordinate>(new GeoPosition<GeoCoordinate>(DateTime.Now, new GeoCoordinate(44.448231, 26.082679))));
+        //    OnPositionChanged(new GeoPositionChangedEventArgs<GeoCoordinate>(new GeoPosition<GeoCoordinate>(DateTime.Now, new GeoCoordinate(44.450681, 26.084096))));   
+        //    OnPositionChanged(new GeoPositionChangedEventArgs<GeoCoordinate>(new GeoPosition<GeoCoordinate>(DateTime.Now, new GeoCoordinate(44.451187, 26.084364))));
+        //}
+
+        #region Implementation of IGeoPositionWatcher<GeoCoordinate>
+
+        public void Start()
+        {
+            
         }
 
-        public new void Stop()
+        public void Start(bool suppressPermissionPrompt)
         {
-            OnPositionStatusChanged(new GeoPositionStatusChangedEventArgs(GeoPositionStatus.Disabled));
         }
+
+        public bool TryStart(bool suppressPermissionPrompt, TimeSpan timeout)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            
+        }
+
+        public GeoPosition<GeoCoordinate> Position { get; private set; }
+        public GeoPositionStatus Status { get; private set; }
+        public event EventHandler<GeoPositionChangedEventArgs<GeoCoordinate>> PositionChanged;
+        public event EventHandler<GeoPositionStatusChangedEventArgs> StatusChanged;
+
+        #endregion
+
+        #region Implementation of IDisposable
+
+        public void Dispose()
+        {
+            
+        }
+
+        #endregion
+
+        #region Implementation of IGeoCoordinateWatcher
+
+        public GeoPositionAccuracy DesiredAccuracy { get; private set; }
+        public double MovementThreshold { get; set; }
+        public GeoPositionPermission Permission { get; private set; }
+
+        #endregion
     }
 }
